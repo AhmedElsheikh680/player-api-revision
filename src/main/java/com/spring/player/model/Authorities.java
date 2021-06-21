fc4028204a4deb37df1d3c06ec0e5e9ea764441a
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-21 22:16:12 +0200

    adding model with annotation

---
+++ src/main/java/com/spring/player/model/Authorities.java
@@ -0,0 +1,52 @@
+package com.spring.player.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authorities")
+public class Authorities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "authoritize")
+    private String authorize;
+
+    @ManyToMany(mappedBy = "authorities")
+    private List<User> users = new ArrayList<>();
+
+    public Authorities() {
+    }
+
+    public Authorities(String authorize) {
+        this.authorize = authorize;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthorize() {
+        return authorize;
+    }
+
+    public void setAuthorize(String authorize) {
+        this.authorize = authorize;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/spring/player/model/User.java
@@ -0,0 +1,108 @@
+package com.spring.player.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "age")
+    private String age;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "active")
+    private int active;
+
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_authorities",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authorities_id")}
+    )
+    private List<Authorities> authorities = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String email, String password, String age, String address, int active) {
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.address = address;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+
+    public List<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authorities> authorities) {
+        this.authorities = authorities;
+    }
+}

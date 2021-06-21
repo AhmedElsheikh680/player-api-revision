35e77af3d2523119cc3d0a568f2107ff636b1323
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-21 21:53:30 +0200

    init project

---
+++ src/main/java/com/spring/player/PlayerApiRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApiRevisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerApiRevisionApplication.class, args);
+    }
+
+}
